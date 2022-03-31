527a924dccfe56f48271c216507c83c1b9feb46d
jj16791 <jj16791@bristol.ac.uk>
2022-03-31 21:26:19 +0100

    Fixed getUpdateState function headers

--- test/unit/MockArchitecture.hh
+++ test/unit/MockArchitecture.hh
@@ -21,6 +21,7 @@ class MockArchitecture : public arch::Architecture {
                          const std::shared_ptr<Instruction>& instruction,
                          const Core& core, MemoryInterface& memory));
   MOCK_CONST_METHOD0(getInitialState, arch::ProcessStateChange());
+  MOCK_CONST_METHOD0(getUpdateState, arch::ProcessStateChange());
   MOCK_CONST_METHOD0(getMaxInstructionSize, uint8_t());
   MOCK_CONST_METHOD0(getVCTreg, simeng::Register());
   MOCK_CONST_METHOD0(getPCCreg, simeng::Register());
